f1e32b76f7617f20f50b436f44d7ae26df75a173
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-15 21:16:20 -0400

    chore: initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const app = express();
+
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
+    });
+  })
+  .catch(() => {
+    console.log("Error connecting to MongoDB");
+  });
