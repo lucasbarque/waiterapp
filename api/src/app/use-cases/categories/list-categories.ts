a53aedfeb44c20af3d87cef03bbb6c8c75cdd76f
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-16 00:46:31 -0400

    feat: implement listCategories use-case function to retrieve categories from the database

--- api/src/app/use-cases/categories/list-categories.ts
+++ api/src/app/use-cases/categories/list-categories.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from "express";
+import { Category } from "../../models/Category";
 
-export function listCategories(_request: Request, response: Response) {
-  return response.send("ok");
+export async function listCategories(_request: Request, response: Response) {
+  try {
+    const categories = await Category.find();
+
+    return response.json(categories);
+  } catch (error) {
+    console.log(error);
+    response.sendStatus(500).json({
+      error: "Internal server error",
+    });
+  }
 }
