c8fefb6b0a0db895a8ddffdfb66d835b37d98a0f
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-16 00:46:09 -0400

    feat: enhance createCategory function to handle category creation with error management

--- api/src/app/use-cases/categories/create-category.ts
+++ api/src/app/use-cases/categories/create-category.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import { Category } from "../../models/Category";
 
-export function createCategory(_request: Request, response: Response) {
-  return response.send("ok");
+export async function createCategory(request: Request, response: Response) {
+  try {
+    const { icon, name } = request.body;
+
+    const category = await Category.create({ icon, name });
+
+    return response.status(201).json(category);
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      error: "Internal server error",
+    });
+  }
 }
